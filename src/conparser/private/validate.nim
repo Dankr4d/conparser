9ed83ab06dc458219d9919a56655a3b3fc9dd257
Dankr4d <dude569@freenet.de>
2021-08-07 18:06:57 +0200

    - WIP: Created for each type an own procedure in validate.nim.
    Signed-off-by: Dankr4d <dude569@freenet.de>

--- src/conparser/private/validate.nim
+++ src/conparser/private/validate.nim
@@ -12,45 +12,127 @@ macro sym(n: typed{sym}): untyped =
     )
   )
 
-# proc validatePrefix*(nIdentDefs: NimNode) =
-#   discard
+proc validatePrefix(nIdentDefs, nPragma: NimNode) =
+  if nPragma == nil:
+    return
+  for nPragmaChild in nPragma.children:
+    if nPragmaChild[0] == sym(Prefix):
+      # Prefix is not allowed on attributes
+      error("Prefix is not allowed on attributes.", nPragmaChild[0])
 
-proc validateObject*(nIdentDefs: NimNode) =
-    var nPragma: NimNode = nIdentDefs.findChild(it.kind == nnkPragmaExpr).findChild(it.kind == nnkPragma)
-    echo nIdentDefs.treeRepr
-    if nPragma == nil and nIdentDefs[1].getTypeImpl().kind != nnkObjectTy:
-      return
+proc validateObject(nIdentDefs, nPragma: NimNode) =
+  if nPragma == nil and nIdentDefs[1].getTypeImpl().kind != nnkObjectTy:
+    return
 
-    # Search Format pragma in attribute
-    var foundFormat: bool = false
-    for nPragmaChild in nPragma.children:
-      if nPragmaChild[0] == sym(Format):
-        foundFormat = true # Found pragma at Attribute
+  # Search Format pragma in attribute
+  var foundFormat: bool = false
+  for nPragmaChild in nPragma.children:
+    if nPragmaChild[0] == sym(Format):
+      foundFormat = true # Found pragma at Attribute
 
-    if nIdentDefs[1].getTypeImpl().kind != nnkObjectTy:
-      if foundFormat:
-        error("Format pragma is only allowed on attributes of type object.", nPragma)
-      return # Not an object
+  if nIdentDefs[1].getTypeImpl().kind != nnkObjectTy:
+    if foundFormat:
+      error("Format pragma is only allowed on attributes of type object.", nPragma)
+    return # Not an object
 
-    # Search Format pragma in object, if Format not already found.
-    let nAttrImpl: NimNode = nIdentDefs[1].getImpl()
-    if not foundFormat:
-      let nAttrImplPragma: NimNode = nAttrImpl.findChild(it.kind == nnkPragmaExpr).findChild(it.kind == nnkPragma)
-      if nAttrImplPragma != nil:
-        for nPragmaChild in nAttrImplPragma.children:
-          if nPragmaChild[0] == sym(Format):
-            foundFormat = true
-            break
+  # Search Format pragma in object, if Format not already found.
+  let nAttrImpl: NimNode = nIdentDefs[1].getImpl()
+  if not foundFormat:
+    let nAttrImplPragma: NimNode = nAttrImpl.findChild(it.kind == nnkPragmaExpr).findChild(it.kind == nnkPragma)
+    if nAttrImplPragma != nil:
+      for nPragmaChild in nAttrImplPragma.children:
+        if nPragmaChild[0] == sym(Format):
+          foundFormat = true
+          break
 
-    if not foundFormat:
-      # Format pragma not found on attribute and object
-      error("Format pragma is missing on attribute or object declaration.", nIdentDefs[0])
+  if not foundFormat:
+    # Format pragma not found on attribute and object
+    error("Format pragma is missing on attribute or object declaration.", nIdentDefs[0])
 
-    # Check rekursively each type implementation of each attribute
-    for nIdentDefs in nAttrImpl.findChild(it.kind == nnkObjectTy).findChild(it.kind == nnkRecList):
-      validateObject(nIdentDefs)
+proc validateDefault(nIdentDefs, nPragma: NimNode) =
+  if nPragma == nil:
+    return
 
+  for nPragmaChild in nPragma.children:
+    if nPragmaChild[0] != sym(Default):
+      continue
 
+    var nTypeInst: NimNode
+    if nIdentDefs[1].kind == nnkBracketExpr and nIdentDefs[1][0].getTypeInst() == getTypeInst(range):
+      nTypeInst = nIdentDefs[1][1][1].getTypeInst()
+    else:
+      nTypeInst = nIdentDefs[1].getTypeInst()
+    let nPragmaDefaultTypeInst: NimNode = nPragmaChild[1].getTypeInst()
+    if nTypeInst != nPragmaDefaultTypeInst:
+      error("Invalid Default value. You passed " & $nPragmaDefaultTypeInst & ", but expected " & $nTypeInst & ".", nPragmaChild[1])
+
+    var isLower, isHigher: bool = false
+    if nIdentDefs[1].kind == nnkBracketExpr and nIdentDefs[1][0].getTypeInst() == getTypeInst(range):
+      let nMinVal: NimNode = nIdentDefs[1][1][1]
+      let nMaxVal: NimNode = nIdentDefs[1][1][2]
+      case nTypeInst.typeKind:
+      of ntyInt..ntyInt64:
+        isLower = nPragmaChild[1].intVal < nMinVal.intVal
+        isHigher = nPragmaChild[1].intVal > nMaxVal.intVal
+      of ntyUInt..ntyUInt64:
+        isLower = nPragmaChild[1].intVal < nMinVal.intVal # TODO: Cast to uintXX type
+        isHigher = nPragmaChild[1].intVal > nMaxVal.intVal # TODO: Cast to uintXX type
+      of ntyFloat..ntyFloat128:
+        isLower = nPragmaChild[1].floatVal < nMinVal.floatVal
+        isHigher = nPragmaChild[1].floatVal > nMaxVal.floatVal
+      else:
+        error("Range type " & $nTypeInst & " is not implemented!", nIdentDefs[1][1])
+      if isLower:
+        error("Default value is lower than specified in range.", nPragmaChild[1])
+      elif isHigher:
+        error("Default value is higher than specified in range.", nPragmaChild[1])
+
+proc validateValid(nIdentDefs, nPragma: NimNode) =
+  if nPragma == nil:
+    return
+
+  for nPragmaChild in nPragma.children:
+    if nPragmaChild[0] != sym(Valid):
+      continue
+
+    var nTypeInst: NimNode
+    if nIdentDefs[1].kind == nnkBracketExpr and nIdentDefs[1][0].getTypeInst() == getTypeInst(range):
+      nTypeInst = nIdentDefs[1][1][1].getTypeInst()
+    else:
+      nTypeInst = nIdentDefs[1].getTypeInst()
+    let nPragmaValidInst: NimNode = nPragmaChild[1].getTypeInst()
+    if nTypeInst != getTypeInst(bool):
+      error("Valid is not allowed at attribute of type " &  $nTypeInst & ".", nPragmaChild[1])
+
+    if nPragmaValidInst == getTypeInst(Bools):
+      var foundTrue, foundFalse: bool = false
+      for idx, nChild in nPragmaChild[1].pairs:
+        if idx == 0:
+          continue # Skip Sym "Bools"
+        case nChild[0].strVal:
+        of "true":
+          foundTrue = true
+        of "false":
+          foundFalse = true
+        else:
+          continue
+        if nChild[1][1].findChild(it.kind == nnkStrLit) == nil:
+          error("Empty seq at Bools constructor parameter `" & nChild[0].strVal & "`.", nChild[1][1])
+      if not foundTrue or not foundFalse:
+        error("Empty seq at Bools constructor parameter of `true` or/and `false`.", nPragmaChild[1])
+
+proc validateAttributes(nAttrRecList: NimNode) =
+  for identDefs in nAttrRecList.children:
+    let nPragma: NimNode = identDefs.findChild(it.kind == nnkPragmaExpr).findChild(it.kind == nnkPragma)
+
+    validatePrefix(identDefs, nPragma)
+    validateObject(identDefs, nPragma)
+    validateDefault(identDefs, nPragma)
+    validateValid(identDefs, nPragma)
+
+    # Call recursivly this function for each attribute with type object
+    if identDefs[1].getTypeImpl().kind == nnkObjectTy:
+      validateAttributes(identDefs[1].getImpl().findChild(it.kind == nnkObjectTy).findChild(it.kind == nnkRecList))
 
 macro validate*(tdesc: typedesc): untyped =
   let impl: NimNode = tdesc.getTypeInst()[1].getImpl()
@@ -62,66 +144,4 @@ macro validate*(tdesc: typedesc): untyped =
       error(nExprColonExpr[0].strVal & " not allowed in object annotation.", nExprColonExpr[0])
 
   # Validate object attribute pragmas
-  let nAttrRecList: NimNode = impl[2][2] #.findChild(it.kind == nnkObjectTy).findChild(it.kind == nnkRecList)
-  for identDefs in nAttrRecList.children:
-    let nAttrPragma: NimNode = identDefs[0][1] #.findChild(it.kind == nnkPragmaExpr).findChild(it.kind == nnkPragma)
-    var nAttrTypeSym: NimNode = identDefs[1]
-    var nkAttrType: NimNodeKind = nAttrTypeSym.getType().kind
-
-    for nExprColonExpr in nAttrPragma.children:
-      var nAttrTypeInst: NimNode
-      if nAttrTypeSym.kind == nnkBracketExpr and nAttrTypeSym[0].getTypeInst() == getTypeInst(range):
-        nAttrTypeInst = nAttrTypeSym[1][1].getTypeInst()
-      else:
-        nAttrTypeInst = nAttrTypeSym.getTypeInst()
-      let nAttrPragmaTypeInst: NimNode = nExprColonExpr[1].getTypeInst()
-
-      if nExprColonExpr[0] == sym(Prefix):
-        # Prefix is not allowed on attributes
-        error("Prefix is not allowed on attributes.", nExprColonExpr[0])
-      elif nExprColonExpr[0] == sym(Default):
-        # Validate if `Default` value type is the same as type of attribute
-        if nAttrTypeInst != nAttrPragmaTypeInst:
-          error("Invalid Default value. You passed " & $nAttrPragmaTypeInst & ", but expected " & $nAttrTypeInst & ".", nExprColonExpr[1])
-
-        var isLower, isHigher: bool = false
-        if nAttrTypeSym.kind == nnkBracketExpr and nAttrTypeSym[0].getTypeInst() == getTypeInst(range):
-          let nMinVal: NimNode = nAttrTypeSym[1][1]
-          let nMaxVal: NimNode = nAttrTypeSym[1][2]
-          case nAttrTypeInst.typeKind:
-          of ntyInt..ntyInt64:
-            isLower = nExprColonExpr[1].intVal < nMinVal.intVal
-            isHigher = nExprColonExpr[1].intVal > nMaxVal.intVal
-          of ntyUInt..ntyUInt64:
-            isLower = nExprColonExpr[1].intVal < nMinVal.intVal # TODO: Cast to uintXX type
-            isHigher = nExprColonExpr[1].intVal > nMaxVal.intVal # TODO: Cast to uintXX type
-          of ntyFloat..ntyFloat128:
-            isLower = nExprColonExpr[1].floatVal < nMinVal.floatVal
-            isHigher = nExprColonExpr[1].floatVal > nMaxVal.floatVal
-          else:
-            error("Range type " & $nAttrTypeInst & " is not implemented!", nAttrTypeSym[1])
-          if isLower:
-            error("Default value is lower than specified in range.", nExprColonExpr[1])
-          elif isHigher:
-            error("Default value is higher than specified in range.", nExprColonExpr[1])
-      elif nExprColonExpr[0] == sym(Valid):
-        if nAttrTypeInst != getTypeInst(bool):
-          error("Valid is not allowed at attribute of type " &  $nAttrTypeInst & ".", nExprColonExpr[1])
-        if nAttrPragmaTypeInst == getTypeInst(Bools):
-          var foundTrue, foundFalse: bool = false
-          for idx, nChild in nExprColonExpr[1].pairs:
-            if idx == 0:
-              continue # Skip Sym "Bools"
-            case nChild[0].strVal:
-            of "true":
-              foundTrue = true
-            of "false":
-              foundFalse = true
-            else:
-              continue
-            if nChild[1][1].findChild(it.kind == nnkStrLit) == nil:
-              error("Empty seq at Bools constructor parameter `" & nChild[0].strVal & "`.", nChild[1][1])
-          if not foundTrue or not foundFalse:
-            error("Empty seq at Bools constructor parameter of `true` or `false`.", nExprColonExpr[1])
-
-    validateObject(identDefs)
+  validateAttributes(impl.findChild(it.kind == nnkObjectTy).findChild(it.kind == nnkRecList))
